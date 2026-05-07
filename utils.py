efe4a60f8551ad0dfc02d85ea9854cac400f652c
govindbasu <govindbasu2005@gmail.com>
2026-05-07 11:33:26 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def validate_password(password):
-    if len(password) < 6:
-        raise ValueError("Password must be at least 6 characters")
-    return True
-
-def encrypt_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
